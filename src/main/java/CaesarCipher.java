ab926ea818467cc3d54057c69025b4fd0af7f343
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:45:31 +0300

    Add the changes for the main method

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,7 +1,13 @@
 public class CaesarCipher {
-
     //Driver class
     public static void main (String [] args) {
         System.out.println("This is the main method");
+        String text = "HI";
+        int s = 2;
+        System.out.println("Original String  : " + text);
+        System.out.println("Key : " + s);
+        System.out.println("Encoded: " + Encrypt.encrypt(text, s));
+        System.out.println("Decoded: " + Encrypt.encrypt(text, s));
+
     }
 }
