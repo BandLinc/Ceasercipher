c8f8373fe4312e9075f739c4a454483e0d8a689a
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:37:03 +0300

    implement full method for decryption

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,31 @@
-public class Decrypt {
+public class Decrypt
+{
+
+    //Add method for decrypting
+    public static StringBuffer decrypt (String text, int s) {
+        s=26-s;
+        //Initialize the string buffer result
+        StringBuffer result=new StringBuffer();
+        if (text.isEmpty() || text==null) {
+            result.append("Input string cannot be null or empty");
+        } else {
+            if (s < 1 || s > 25) {
+                result.append("Key length should be between 1 and 25");
+            } else {
+                for (int i = 0; i < text.length(); i++) {
+                    if (Character.isUpperCase(text.charAt(i))) {
+                        char ch = (char) (((int) text.charAt(i) +
+                                s - 65) % 26 + 65);
+                        result.append(ch);
+                    } else {
+                        char ch = (char) (((int) text.charAt(i) +
+                                s - 97) % 26 + 97);
+                        result.append(ch);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
 }
