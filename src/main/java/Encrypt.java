b18ba7898ada98fe067ac7fadff27a55c7fd8ce0
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:22:39 +0300

    implement the condition to check for input string null or empty

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -3,24 +3,24 @@ public class Encrypt {
     public static StringBuffer encrypt (String text, int s) {
         //Initialize the string buffer result
         StringBuffer result=new StringBuffer();
-        if (s<1 || s>25) {
-            result.append("Key length should be between 1 and 25");
+        if (text.isEmpty() || text==null) {
+            result.append("Input string cannot be null or empty");
         } else {
-        for (int i=0; i<text.length(); i++)
-        {
-            if (Character.isUpperCase(text.charAt(i)))
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 65) % 26 + 65);
-                result.append(ch);
+            if (s < 1 || s > 25) {
+                result.append("Key length should be between 1 and 25");
+            } else {
+                for (int i = 0; i < text.length(); i++) {
+                    if (Character.isUpperCase(text.charAt(i))) {
+                        char ch = (char) (((int) text.charAt(i) +
+                                s - 65) % 26 + 65);
+                        result.append(ch);
+                    } else {
+                        char ch = (char) (((int) text.charAt(i) +
+                                s - 97) % 26 + 97);
+                        result.append(ch);
+                    }
+                }
             }
-            else
-            {
-                char ch = (char)(((int)text.charAt(i) +
-                        s - 97) % 26 + 97);
-                result.append(ch);
-            }
-        }
         }
         return result;
     }
