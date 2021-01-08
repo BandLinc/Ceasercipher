40b4880b23f000c86fcc4cba367a2fefcd9c1e59
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:02:26 +0300

    implement the method to check for the encryption of the key

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -4,9 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
 
+    //Checking the encryption is returning the correct value
     @Test
     void encryption () {
         assertEquals(Encrypt.encrypt("HI",2).toString(),"JK");
+    }
+
+    //Checking that value submitted as key is between 1 and 25
+    @Test
+    void checkKey () {
+
     }
 
 }
