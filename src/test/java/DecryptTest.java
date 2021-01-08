fd3e68f747edcf8b78923394d4b7934ad46ecf85
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:39:40 +0300

    implement method to check decryption is working fine

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecryptTest {
-
+    //Checking the decryption is returning the correct value
+    @Test
+    void decryption () {
+        assertEquals(Decrypt.decrypt("JK",2).toString(),"HI");
+    }
 }
