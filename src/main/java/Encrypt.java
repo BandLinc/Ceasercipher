d16b7f36bf72f10a4d144e8907e2d3ddb5c2c660
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 17:48:04 +0300

    implement the encryption method and include the return type

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -3,6 +3,21 @@ public class Encrypt {
     public static StringBuffer encrypt (String text, int s) {
         //Initialize the string buffer result
         StringBuffer result=new StringBuffer();
-        return  result;
+        for (int i=0; i<text.length(); i++)
+        {
+            if (Character.isUpperCase(text.charAt(i)))
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                        s - 65) % 26 + 65);
+                result.append(ch);
+            }
+            else
+            {
+                char ch = (char)(((int)text.charAt(i) +
+                        s - 97) % 26 + 97);
+                result.append(ch);
+            }
+        }
+        return result;
     }
 }
