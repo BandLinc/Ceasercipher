ff28aabf2b27d3bff8ae01433d58fbc3426861bf
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:14:50 +0300

    implement test to check for key length below 1

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -3,6 +3,9 @@ public class Encrypt {
     public static StringBuffer encrypt (String text, int s) {
         //Initialize the string buffer result
         StringBuffer result=new StringBuffer();
+        if (s<1 || s>25) {
+            result.append("Key length should be between 1 and 25");
+        } else {
         for (int i=0; i<text.length(); i++)
         {
             if (Character.isUpperCase(text.charAt(i)))
@@ -17,6 +20,7 @@ public class Encrypt {
                         s - 97) % 26 + 97);
                 result.append(ch);
             }
+        }
         }
         return result;
     }
