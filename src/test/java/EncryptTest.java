53c7af22e88f9eae5f6d68323e06e8cbc481d4ef
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 18:22:05 +0300

    implement test to check for for input string null or empty

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -16,4 +16,16 @@ class EncryptTest {
         assertEquals(Encrypt.encrypt("HI",26).toString(),"Key length should be between 1 and 25");
     }
 
+    //Checking for the key below 1
+    @Test
+    void checkKeyLengthBelow1 () {
+        assertEquals(Encrypt.encrypt("HI",-1).toString(),"Key length should be between 1 and 25");
+    }
+
+    //check if the string is null or empty
+    @Test
+    void checkIfNullOrEmptyString () {
+        assertEquals(Encrypt.encrypt("",4).toString(),"Input string cannot be null or empty");
+    }
+
 }
