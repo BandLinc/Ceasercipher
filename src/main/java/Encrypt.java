28096c00f20a13959d6a0c303289c7f66390375d
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 17:39:08 +0300

    Add the encrypt method in the encryption class

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,3 +1,8 @@
 //This class is used for encryption
 public class Encrypt {
+    public static StringBuffer encrypt (String text, int s) {
+        //Initialize the string buffer result
+        StringBuffer result=new StringBuffer();
+        return  result;
+    }
 }
