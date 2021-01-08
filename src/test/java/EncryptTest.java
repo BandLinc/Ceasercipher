de217ef2ae5380210db80577baec8eb8ce2c938c
LincolnLidanya <lincolnlidanya@gmail.com>
2021-01-08 17:44:46 +0300

    implement the encryption test method in the encryption test class

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
+
+    @Test
+    void encryption () {
+        assertEquals(Encrypt.encrypt("HI",2),"JK");
+    }
 
 }
